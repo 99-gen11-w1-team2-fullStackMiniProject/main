6b4f0348edc058f7a3155d72a36d822277cca2b2
jinhwan7 <rkflql851@gmail.com>
2022-12-10 11:28:05 +0900

    no message

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ db = client.cofee
 @app.route('/')
 def home():
    return render_template('login.html')
-@app.route('/main')
-def main():
-    return render_template('main.html')
+
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
