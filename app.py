f34fa29be213885695e1138f0c7e345857cdad9f
IOTrue <kjoonc.dev@gmail.com>
2022-12-13 01:32:14 +0900

    글 작성시  작성 카테고리로 랜딩

--- app.py
+++ app.py
@@ -346,12 +346,14 @@ def likeToggle():
     return jsonify({'likeToggle': postIndex_receive + ' '})
 
 # 삭제
+
+
 @app.route("/delete", methods=["POST"])
 def delete_btn():
     num = request.form['num_give']
     print(type(num))
     num1 = int(num)
-    db.post.delete_one({'num':num1})
+    db.post.delete_one({'num': num1})
 
     return redirect(url_for("home"))
 
